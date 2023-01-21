f100cb1feb3c6ed40ca16b5a6fdc39a75412b645
Ben <bdc1g19@soton.ac.uk>
2023-01-21 16:34:15 +0000

    remove build dir

--- firmware/cmake-build-zdebug/image_preload.cmake
+++
@@ -1,4 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD_ROOT "/Users/ben/Education/University/Year4/Project/Firmware;/opt/nordic/ncs/v2.1.0/nrf;/opt/nordic/ncs/v2.1.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "prj.ZDebug.conf;prj.common.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/Users/ben/Education/University/Year4/Project/firmware/boards/vital_monitor_nrf52832.overlay" CACHE INTERNAL "NCS child image controlled")
